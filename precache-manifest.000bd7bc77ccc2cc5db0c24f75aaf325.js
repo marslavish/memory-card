a0d16190d227f457b008aa621b3c5ce00a0c48e3
marslavish <uniqueye608@gmail.com>
2020-10-18 12:22:23 +0800

    Updates

---
+++ precache-manifest.000bd7bc77ccc2cc5db0c24f75aaf325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20041c306f563559746bb939c5a87c23",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "009fbc0f312411a5f949",
+    "url": "/memory-card/static/css/main.ebc835db.chunk.css"
+  },
+  {
+    "revision": "06b44682e8c4d75256cc",
+    "url": "/memory-card/static/js/2.4d605996.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/memory-card/static/js/2.4d605996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009fbc0f312411a5f949",
+    "url": "/memory-card/static/js/main.40062aa7.chunk.js"
+  },
+  {
+    "revision": "9f239ed59a6dab3935f2",
+    "url": "/memory-card/static/js/runtime-main.83cd4e43.js"
+  }
+]);
